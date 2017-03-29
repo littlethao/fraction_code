3ff15f4d2ffd52fca19d25bf50aed0928ee49275
thaovo <ttthao.vo@gmail.com>
2017-03-29 23:33:24 +0100

    implemented functionality to calculate addition of two fractions

--- lib/fraction_calculator.rb
+++ lib/fraction_calculator.rb
@@ -1,2 +1,11 @@
 class FractionCalculator
+
+  def calculate_fraction(fraction_one, fraction_two, operator)
+    first_fraction = fraction_one.to_r
+    second_fraction = fraction_two.to_r
+
+    if operator == '+'
+      return (first_fraction + second_fraction).to_s
+    end
+  end
 end
--- spec/fraction_calculator_spec.rb
+++ spec/fraction_calculator_spec.rb
@@ -1,5 +1,12 @@
 require "fraction_calculator"
 
 describe FractionCalculator do
+  subject(:fraction_calculator) { described_class.new }
 
+  context 'addition' do
+    it 'adds the second fraction to the first fraction and returns the result as fraction' do
+        result = fraction_calculator.calculate_fraction("1/3", "2/4", "+")
+        expect(result).to eq("5/6")
+    end
+  end
 end
