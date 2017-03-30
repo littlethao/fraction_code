d3885083b56008c3e0900b057720037aa00acfb6
Thao Vo <thao@Thaos-Mac-mini.local>
2017-03-30 12:43:17 +0100

    triangulated - 1 + 0 = 1

--- spec/fraction_spec.rb
+++ spec/fraction_spec.rb
@@ -1,13 +1,14 @@
 describe "adding fractions" do
   describe "0 + 0" do
     class Fraction
-
+      attr_reader :numerator
+      
       def initialize(numerator, denominator)
-
+        @numerator = numerator
       end
 
       def +(fraction)
-        0
+        numerator
       end
     end
 
@@ -16,4 +17,12 @@ describe "adding fractions" do
       expect(sum).to eq(0)
     end
   end
+
+  describe "1 + 0" do
+    it "equals 1" do
+      sum = Fraction.new(1, 1) + Fraction.new(0, 1)
+      expect(sum).to eq(1)
+    end
+  end
+
 end
