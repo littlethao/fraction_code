2f2156d7a85dd5515bb46bebf381691a591879bb
Thao Vo <thao@Thaos-Mac-mini.local>
2017-04-11 13:13:39 +0100

    1/4 + 1/8 = 3/8 and 1/9 + 1/3 = 4/9 and 1/4 + 1/3 = 7/12

--- spec/fraction_spec.rb
+++ spec/fraction_spec.rb
@@ -1,14 +1,15 @@
 describe "adding fractions" do
   class Fraction
     attr_reader :numerator, :denominator
-    
+
     def initialize(numerator, denominator)
       @numerator = numerator
       @denominator = denominator
     end
 
     def +(fraction)
-      Fraction.new(numerator + fraction.numerator, denominator.lcm(fraction.denominator))
+      lcm = denominator.lcm(fraction.denominator)
+      Fraction.new(numerator * (lcm/denominator) + (lcm/fraction.denominator) * fraction.numerator, lcm)
     end
 
     def to_i
@@ -17,7 +18,7 @@ describe "adding fractions" do
   end
 
   describe "0 + 0" do
-    it "equals 0" do 
+    it "equals 0" do
       sum = Fraction.new(0, 1) + Fraction.new(0, 1)
       expect(sum.to_i).to eq(0)
     end
@@ -64,6 +65,38 @@ describe "adding fractions" do
       sum = Fraction.new(0, 1) + Fraction.new(1, 2)
       expect(sum.numerator).to eq(1)
       expect(sum.denominator).to eq(2)
+    end
+  end
+
+  describe "1/3 + 1/3" do
+    it "equals 2/3" do
+      sum = Fraction.new(1, 3) + Fraction.new(1, 3)
+      expect(sum.numerator).to eq(2)
+      expect(sum.denominator).to eq(3)
+    end
+  end
+
+  describe "1/4 + 1/8" do
+    it "equals 3/8" do
+      sum = Fraction.new(1, 4) + Fraction.new(1, 8)
+      expect(sum.numerator).to eq(3)
+      expect(sum.denominator).to eq(8)
+    end
+  end
+
+  describe "1/9 + 1/3" do
+    it "equals 4/9" do
+      sum = Fraction.new(1, 9) + Fraction.new(1, 3)
+      expect(sum.numerator).to eq(4)
+      expect(sum.denominator).to eq(9)
+    end
+  end
+
+  describe "1/4 + 1/3" do
+    it "equals 7/12" do
+      sum = Fraction.new(1, 4) + Fraction.new(1, 3)
+      expect(sum.numerator).to eq(7)
+      expect(sum.denominator).to eq(12)
     end
   end
 end
